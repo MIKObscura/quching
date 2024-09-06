7651bb852771d35f7ad42f9add6218da7cc03ddb
MIKObscura <contact.mikobscura@gmail.com>
2024-09-06 11:17:39 +0200

    added config init function

--- main.py
+++ main.py
@@ -2,18 +2,27 @@ from quching.quching_window import QuchingWindow
 from quching.quching_player import QuchingPlayer
 from quching.indexer.index import refresh_index
 import sys
+import os
+from pathlib import Path
 from multiprocessing import Process
 from PySide6.QtWidgets import QApplication
+
+def init_config_dir():
+    Path(os.path.join(str(Path("~").expanduser()), ".config/quching")).mkdir(parents=True, exist_ok=True)
+    Path(os.path.join(str(Path("~").expanduser()), ".config/quching/playlists")).mkdir(parents=True, exist_ok=True)
+    open(os.path.join(str(Path("~").expanduser()), ".config/quching/blacklist.txt"), "w").close()
 
 if __name__=="__main__":
     app = QApplication()
 
+    if not os.path.exists(os.path.join(str(Path("~").expanduser()), ".config/quching")):
+        init_config_dir()
     player = QuchingPlayer(sys.argv[1:], 0)
     quching = QuchingWindow(player)
-    # indexer = Process(target=refresh_index, daemon=True)
+    indexer = Process(target=refresh_index, daemon=True)
     quching.show()
-    # indexer.start()
+    indexer.start()
 
     app.exec()
 
-    # indexer.join()
+    indexer.join()
